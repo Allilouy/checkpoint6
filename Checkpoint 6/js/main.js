5cb3d2b297fa80a1dbcdf22e1f8647dd8a9958b0
Allilouy <47003777+Allilouy@users.noreply.github.com>
2019-02-17 16:49:31 +0100

    Add files via upload

--- Checkpoint 6/js/main.js
+++ Checkpoint 6/js/main.js
@@ -1,11 +1,5 @@
-
-$('#submit').click(function () { 
-   window.open('form.html');
-   window.close(this)
-   });
+  
 
 if (confirm('You can only proceed if you have an ADA account')) {
 } else {
-    window.close();
-}
-
+    window.close();}
